839753491d303d522aede7a77448bd09e86324b6
brunoguttierrez <brunoguttierrezfn@gmail.com>
2021-09-22 00:14:05 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,11 +5,43 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-        /*int a = 5;
-        int b = 6;
-
-                    System.out.println("Hello" + (a+b));*/
-
         System.out.println(gato);
+
+
+        Livro livro = new Livro("Teste", 300);
+        System.out.println(livro);
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numPags;
+
+    public Livro(String nome, Integer numPags) {
+        this.nome = nome;
+        this.numPags = numPags;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPags() {
+        return numPags;
+    }
+
+    public void setNumPags(Integer numPags) {
+        this.numPags = numPags;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPags=" + numPags +
+                '}';
     }
 }
